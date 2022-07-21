bae232d6526a4383143fc3cffbb1684698fa5ece
kkk125504 <kginyc5504@gmail.com>
2022-07-21 20:31:45 +0900

    게시물 테스트데이터 생성, article생성자 변경

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -5,12 +5,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+   private static List<Article> articles;
+   
+   static { articles = new ArrayList<>();
+   }
+   
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-		int lastArticleId = 0;
-		List<Article> articles = new ArrayList<>();
-		while (true) {
+	
+		makeTestData();
+		
+		while (true) {						
 			System.out.printf("명령어 ) ");
 			String cmd = sc.nextLine().trim();
 			if (cmd.length() == 0) {
@@ -21,8 +27,7 @@ public class Main {
 				break;
 			}
 			if (cmd.equals("article write")) {
-				int id = lastArticleId + 1;
-				lastArticleId = id;
+				int id = articles.size() + 1;
 				String regDate = Util.getNowDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
@@ -123,6 +128,15 @@ public class Main {
 		System.out.println("==프로그램 끝==");
 		sc.close();
 	}
+
+	private static void makeTestData() {
+		System.out.println("테스트를 위한 게시물 데이터를 생성합니다.");
+		articles.add(new Article(1, Util.getNowDateStr(), "제목1", "내용1", 11));
+		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 454));
+		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 126));
+	}
+	
+	
 }
 
 class Article {
@@ -133,10 +147,14 @@ class Article {
 	int hit;
 	
 	Article(int id, String regDate, String title, String body) {
+		this(id,regDate,title,body,0);
+	}
+	Article(int id, String regDate, String title, String body, int hit) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.hit = hit;
 	}
 	
 	public void increaseHit() {
