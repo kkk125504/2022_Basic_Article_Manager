ba480d8f613b17c6adbd21a222b0c614fcb44706
kkk125504 <kginyc5504@gmail.com>
2022-07-22 21:32:32 +0900

    컨트롤러에 테스트데이터(추상메서드)로 구현하도록함

--- src/com/KoreaIT/java/BAM/controller/Controller.java
+++ src/com/KoreaIT/java/BAM/controller/Controller.java
@@ -3,4 +3,7 @@ package com.KoreaIT.java.BAM.controller;
 public abstract class Controller {
 	
 	public abstract void doAction(String cmd, String actionMethodName);
+	
+	//테스트 데이터
+	public abstract void makeTestData();
 }
